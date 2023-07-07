e594b84cf20a2ec8f15bd0b4cb78f717b0eef32a
Sujal Acharya <sujalacharya567@gmail.com>
2023-07-07 14:00:48 -0400

    final codes

---
+++ Product/app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  //url: "mongodb://localhost:27017/Blessing_db"
+
+ 
+
+  //ATLAS_URI=mongodb + srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/tutorials?retryWrites=true&w=majority;
+
+   //url:"mongodb+srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/Tutorial?retryWrites=true&w=majority"
+
+url:"mongodb+srv://sujalacharya567:hahaha123@assignment2.kot4riu.mongodb.net/DressStore?retryWrites=true&w=majority"
+
+};
